8ca475f5af43d96bc1532408f7945442d7e4612e
martineng <martin.eca.6651@gmail.com>
2018-05-22 23:28:46 +0800

    First Commit

---
+++ databaseDLL/databaseConnection.cs
@@ -0,0 +1,25 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace databaseDLL
+{
+    public class databaseConnection
+    {
+        public static MySqlConnection openConnection()
+        {
+            string myConnection = "server=223.27.22.124;" +
+                                   "User Id=martin01;password=l-l3110oO;" +
+                                   "database=041602461_ComHRInvoiceSystem";
+
+            MySqlConnection myConn = new MySqlConnection(myConnection);
+            myConn.Open();
+
+            return myConn;
+        }//END openConenction()
+
+    }
+}
